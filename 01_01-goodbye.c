ec58dc15231547b0805adc45a67a3a2c6807f4e5
Tyler Black <145217103+blackt117@users.noreply.github.com>
2024-01-04 16:24:38 -0500

    Add files via upload

---
+++ 01_01-goodbye.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Generates multiple lines of code
+#define CONST 10
+const int b = 20;
+char char_test = 'A';
+int times;
+int init = 1;
+char title()
+{
+    return "Hello";
+}
+int main()
+{
+	scanf("%d",&times);
+	for (int q =0; q < times; q++)
+    {
+        if (q%2==0)
+        {
+            for(int i=0; i < 7; i++)
+            {
+                char char_test2 = char_test + i;
+                printf("%d%c ",init,(char_test+i));
+            }
+            printf("\n");
+            init = init + 1;
+        }
+        else
+        {
+            for (int t = 0; t < b; t++)
+            {
+                printf("=");
+            }
+            printf("\n");
+        }
+    }
+    printf("%s",title());
+	return 0;
+}
